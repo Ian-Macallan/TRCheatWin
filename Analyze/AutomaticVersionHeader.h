ad04a5da37060bd2aa0068be22211eceec4a7e60
Macallan <ian.macallan@gmail.com>
2024-05-23 09:49:12 +0200

    Release

--- Analyze/AutomaticVersionHeader.h
+++ Analyze/AutomaticVersionHeader.h
@@ -5,7 +5,7 @@
 //
 //	(c) 2003 - 2024 Macallan - mailto:ian.macallan@gmail.com
 //
-//	Generated : 23/05/2024 09:38:46
+//	Generated : 23/05/2024 09:48:16
 //
 //===========================================================================================
 //
@@ -55,22 +55,22 @@
 #endif
 
 #ifdef UNICODE
-#define	PROGRAM_DATE	L"Thu May 23 09:38"
-#define	PROGRAM_DATE_F	L"Thu May 23 09:38 2024"
+#define	PROGRAM_DATE	L"Thu May 23 09:48"
+#define	PROGRAM_DATE_F	L"Thu May 23 09:48 2024"
 #define	PROGRAM_YEAR	L"2024"
-#define	PROGRAM_TIME	L"2024/05/23 09:38:46"
+#define	PROGRAM_TIME	L"2024/05/23 09:48:16"
 #else
-#define	PROGRAM_DATE	"Thu May 23 09:38"
-#define	PROGRAM_DATE_F	"Thu May 23 09:38 2024"
+#define	PROGRAM_DATE	"Thu May 23 09:48"
+#define	PROGRAM_DATE_F	"Thu May 23 09:48 2024"
 #define	PROGRAM_YEAR	"2024"
-#define	PROGRAM_TIME	"2024/05/23 09:38:46"
+#define	PROGRAM_TIME	"2024/05/23 09:48:16"
 #endif
 
 #ifdef UNICODE
-#define	PROGRAM_VERSION	L"4.1.09.004"
-#define	PROGRAM_INSTALL	L"4.1.09004"
-#define	PROGRAM_VER_NUM	40109004
-#define	PROGRAM_BUILD	L"617"
+#define	PROGRAM_VERSION	L"4.1.09.005"
+#define	PROGRAM_INSTALL	L"4.1.09005"
+#define	PROGRAM_VER_NUM	40109005
+#define	PROGRAM_BUILD	L"618"
 #define	PROGRAM_TITLE	L"Analyze"
 #ifdef _WIN64
 #define	PROGRAM_TITLE_P	L"Analyze (x64 Unicode)"
@@ -78,10 +78,10 @@
 #define	PROGRAM_TITLE_P	L"Analyze (x86 Unicode)"
 #endif
 #else
-#define	PROGRAM_VERSION	"4.1.09.004"
-#define	PROGRAM_INSTALL	"4.1.09004"
-#define	PROGRAM_VER_NUM	40109004
-#define	PROGRAM_BUILD	"617"
+#define	PROGRAM_VERSION	"4.1.09.005"
+#define	PROGRAM_INSTALL	"4.1.09005"
+#define	PROGRAM_VER_NUM	40109005
+#define	PROGRAM_BUILD	"618"
 #define	PROGRAM_TITLE	"Analyze"
 #ifdef _WIN64
 #define	PROGRAM_TITLE_P	"Analyze (x64 MBCS)"
--- ReadTR2/AutomaticProductName.h
+++ ReadTR2/AutomaticProductName.h
@@ -1,20 +1,20 @@
 #ifdef _WIN64
 #ifdef UNICODE
 #define	ORIGINAL_FILENAME	L"ReadTR2 (x64 Unicode) (VC11.0/u5)\0"
-#define	PRODUCT_NAME		L"ReadTR2 - Version 4.1.09.004\r\n(Build 341) - (x64 Unicode) (VC11.0/u5)\0"
+#define	PRODUCT_NAME		L"ReadTR2 - Version 4.1.09.005\r\n(Build 342) - (x64 Unicode) (VC11.0/u5)\0"
 #else
 #define	ORIGINAL_FILENAME	"ReadTR2 (x64 MBCS) (VC11.0/u5)\0"
-#define	PRODUCT_NAME		"ReadTR2 - Version 4.1.09.004\r\n(Build 341) - (x64 MBCS) (VC11.0/u5)\0"
+#define	PRODUCT_NAME		"ReadTR2 - Version 4.1.09.005\r\n(Build 342) - (x64 MBCS) (VC11.0/u5)\0"
 #endif
 #elif _WIN32
 #ifdef UNICODE
 #define	ORIGINAL_FILENAME	L"ReadTR2 (x86 Unicode) (VC11.0/u5)\0"
-#define	PRODUCT_NAME		L"ReadTR2 - Version 4.1.09.004\r\n(Build 341) - (x86 Unicode) (VC11.0/u5)\0"
+#define	PRODUCT_NAME		L"ReadTR2 - Version 4.1.09.005\r\n(Build 342) - (x86 Unicode) (VC11.0/u5)\0"
 #else
 #define	ORIGINAL_FILENAME	"ReadTR2 (x86 MBCS) (VC11.0/u5)\0"
-#define	PRODUCT_NAME		"ReadTR2 - Version 4.1.09.004\r\n(Build 341) - (x86 MBCS) (VC11.0/u5)\0"
+#define	PRODUCT_NAME		"ReadTR2 - Version 4.1.09.005\r\n(Build 342) - (x86 MBCS) (VC11.0/u5)\0"
 #endif
 #else
 #define	ORIGINAL_FILENAME	"ReadTR2 (VC11.0/u5)\0"
-#define	PRODUCT_NAME		"ReadTR2 - Version 4.1.09.004\r\n(Build 341) - (VC11.0/u5)\0"
+#define	PRODUCT_NAME		"ReadTR2 - Version 4.1.09.005\r\n(Build 342) - (VC11.0/u5)\0"
 #endif
