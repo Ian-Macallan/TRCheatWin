12cd3a560e4662bafa66ec9a9facf45e33dd57cd
Macallan <ian.macallan@gmail.com>
2025-02-23 10:45:06 +0100

    TRR 6

--- ReadTR2/AutomaticProductName.h
+++ ReadTR2/AutomaticProductName.h
@@ -6,22 +6,22 @@
 #ifdef _WIN64
 #ifdef UNICODE
 #define ORIGINAL_FILENAME   L"ReadTR2 (x64 Unicode) (MSVC)\0"
-#define PRODUCT_NAME        L"ReadTR2 - Version 4.6.02.003\r\n(Build 729) - (x64 Unicode) (MSVC)\0"
+#define PRODUCT_NAME        L"ReadTR2 - Version 4.6.02.004\r\n(Build 730) - (x64 Unicode) (MSVC)\0"
 #else
 #define ORIGINAL_FILENAME   "ReadTR2 (x64 MBCS) (MSVC)\0"
-#define PRODUCT_NAME        "ReadTR2 - Version 4.6.02.003\r\n(Build 729) - (x64 MBCS) (MSVC)\0"
+#define PRODUCT_NAME        "ReadTR2 - Version 4.6.02.004\r\n(Build 730) - (x64 MBCS) (MSVC)\0"
 #endif
 #elif _WIN32
 #ifdef UNICODE
 #define ORIGINAL_FILENAME   L"ReadTR2 (x86 Unicode) (MSVC)\0"
-#define PRODUCT_NAME        L"ReadTR2 - Version 4.6.02.003\r\n(Build 729) - (x86 Unicode) (MSVC)\0"
+#define PRODUCT_NAME        L"ReadTR2 - Version 4.6.02.004\r\n(Build 730) - (x86 Unicode) (MSVC)\0"
 #else
 #define ORIGINAL_FILENAME   "ReadTR2 (x86 MBCS) (MSVC)\0"
-#define PRODUCT_NAME        "ReadTR2 - Version 4.6.02.003\r\n(Build 729) - (x86 MBCS) (MSVC)\0"
+#define PRODUCT_NAME        "ReadTR2 - Version 4.6.02.004\r\n(Build 730) - (x86 MBCS) (MSVC)\0"
 #endif
 #else
 #define ORIGINAL_FILENAME   "ReadTR2 (MSVC)\0"
-#define PRODUCT_NAME        "ReadTR2 - Version 4.6.02.003\r\n(Build 729) - (MSVC)\0"
+#define PRODUCT_NAME        "ReadTR2 - Version 4.6.02.004\r\n(Build 730) - (MSVC)\0"
 #endif
 
 #endif
