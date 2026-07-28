6de3a3a518e6dcbe9bee47a5bedf49c47c913c39
Macallan <ian.macallan@gmail.com>
2026-07-28 09:16:59 +0200

    Minor for TR4NG

--- Analyze/AutomaticVersionHeader.h
+++ Analyze/AutomaticVersionHeader.h
@@ -5,7 +5,7 @@
 //
 //	(c) 2003 - 2026 Macallan - mailto:ian.macallan@gmail.com
 //
-//	Generated : 17/06/2026 09:53:23
+//	Generated : 28/07/2026 09:12:58
 //
 //===========================================================================================
 //
@@ -57,22 +57,22 @@
 #endif
 
 #ifdef UNICODE
-#define PROGRAM_DATE    L"Wed Jun 17 09:53"
-#define PROGRAM_DATE_F  L"Wed Jun 17 09:53 2026"
+#define PROGRAM_DATE    L"Tue Jul 28 09:12"
+#define PROGRAM_DATE_F  L"Tue Jul 28 09:12 2026"
 #define PROGRAM_YEAR    L"2026"
-#define PROGRAM_TIME    L"2026/06/17 09:53:23"
+#define PROGRAM_TIME    L"2026/07/28 09:12:58"
 #else
-#define PROGRAM_DATE    "Wed Jun 17 09:53"
-#define PROGRAM_DATE_F  "Wed Jun 17 09:53 2026"
+#define PROGRAM_DATE    "Tue Jul 28 09:12"
+#define PROGRAM_DATE_F  "Tue Jul 28 09:12 2026"
 #define PROGRAM_YEAR    "2026"
-#define PROGRAM_TIME    "2026/06/17 09:53:23"
+#define PROGRAM_TIME    "2026/07/28 09:12:58"
 #endif
 
 #ifdef UNICODE
-#define PROGRAM_VERSION L"4.7.05.014"
-#define PROGRAM_INSTALL L"4.7.05014"
-#define PROGRAM_VER_NUM 40705014
-#define PROGRAM_BUILD   L"1136"
+#define PROGRAM_VERSION L"4.7.05.015"
+#define PROGRAM_INSTALL L"4.7.05015"
+#define PROGRAM_VER_NUM 40705015
+#define PROGRAM_BUILD   L"1137"
 #define PROGRAM_TITLE   L"Analyze"
 #ifdef _WIN64
 #define PROGRAM_TITLE_P L"Analyze (x64 Unicode)"
@@ -80,10 +80,10 @@
 #define PROGRAM_TITLE_P L"Analyze (x86 Unicode)"
 #endif
 #else
-#define PROGRAM_VERSION "4.7.05.014"
-#define PROGRAM_INSTALL "4.7.05014"
-#define PROGRAM_VER_NUM 40705014
-#define PROGRAM_BUILD   "1136"
+#define PROGRAM_VERSION "4.7.05.015"
+#define PROGRAM_INSTALL "4.7.05015"
+#define PROGRAM_VER_NUM 40705015
+#define PROGRAM_BUILD   "1137"
 #define PROGRAM_TITLE   "Analyze"
 #ifdef _WIN64
 #define PROGRAM_TITLE_P "Analyze (x64 MBCS)"
--- ProductVersionInfo.cmd
+++ ProductVersionInfo.cmd
@@ -3,6 +3,6 @@
 rem cd %~dp0
 rem %~d0
 
-set PRODUCT_VERSION_VERS=4.7.5.14
+set PRODUCT_VERSION_VERS=4.7.5.15
 
 @exit /b
